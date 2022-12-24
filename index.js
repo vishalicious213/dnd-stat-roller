a2727981829047d9633f872ef2433f6baafc0068
vishalicious213 <vishalicious213@gmail.com>
2022-12-24 14:57:54 -0800

    index.js: grabbed stat value elements

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let strength = document.getElementById("str")
+let dexterity = document.getElementById("dex")
+let constitution = document.getElementById("con")
+let intelligence = document.getElementById("int")
+let wisdom = document.getElementById("wis")
+let charisma = document.getElementById("cha")
+
 function rollDice() {
     console.log("DnD Stat Roller")
 }
