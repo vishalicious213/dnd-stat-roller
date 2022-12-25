2246172452bc888a03d8f9556bebc32da7b42fd1
vishalicious213 <vishalicious213@gmail.com>
2022-12-25 01:42:02 -0800

    moved attribute scores to left of attribute names. working out how to render scores properly (getting NaN for some when modifiers are applied because of prepended + for positive numbers

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ let baseIntelligence = document.getElementById("int-base")
 let baseWisdom = document.getElementById("wis-base")
 let baseCharisma = document.getElementById("cha-base")
 
-
 // html attribute modifier elements
 let modStr = document.getElementById("mod-str")
 let modDex = document.getElementById("mod-dex")
@@ -24,6 +23,7 @@ let modWis = document.getElementById("mod-wis")
 let modCha = document.getElementById("mod-cha")
 
 let characterStats = [0, 0, 0, 0, 0, 0]
+let baseCharacterStats = [0, 0, 0, 0, 0, 0]
 
 let characterRace = "human"
 let previousRace = "human"
@@ -45,6 +45,26 @@ function generateStat() {
     return stat
 }
 
+// render BASE stats onscreen
+function renderBaseStats() {
+    baseStrength.textContent = characterStats[0]
+    baseDexterity.textContent = characterStats[1]
+    baseConstitution.textContent = characterStats[2]
+    baseIntelligence.textContent = characterStats[3]
+    baseWisdom.textContent = characterStats[4]
+    baseCharisma.textContent = characterStats[5]
+}
+
+// render stats onscreen
+function renderStats() {
+    strength.textContent = characterStats[0]
+    dexterity.textContent = characterStats[1]
+    constitution.textContent = characterStats[2]
+    intelligence.textContent = characterStats[3]
+    wisdom.textContent = characterStats[4]
+    charisma.textContent = characterStats[5]
+}
+
 // set selected race/species
 function chooseRace(raceFromForm) {
     characterRace = raceFromForm.value // radio button value is a string
@@ -54,21 +74,23 @@ function chooseRace(raceFromForm) {
 // apply racial ability modifiers
 function raceModifier() {
     if (characterRace === "human") {
-        console.log("applyling human modifiers")
+        console.log("applying human modifiers")
+        console.log(raceModifiers.human)
         renderRaceModifiers(0)
-        applyRaceModifiers()
+        applyRaceModifiers(0)
         previousRace = "human"
     }
 
     if (characterRace === "dwarf") {
-        console.log("applyling dwarf modifiers")
-        // console.log(raceModifiers.dwarf)
+        console.log("applying dwarf modifiers")
+        console.log(raceModifiers.dwarf)
         renderRaceModifiers(1)
-        applyRaceModifiers()
+        applyRaceModifiers(1)
         previousRace = "dwarf"
     }
 }
 
+// find which set of race modifiers to render
 function renderRaceModifiers(renderRace) {
     if (renderRace === 0) {
         console.log("render human")
@@ -81,8 +103,9 @@ function renderRaceModifiers(renderRace) {
     }
 }
 
-function renderModifiers(modSet) {
-    let values = modSet
+// render penalties/bonuses in Modifiers column
+function renderModifiers(modifierSet) {
+    let values = modifierSet
     for (let i = 0; i < 6; i++) {
         if (values[i] === 0) {
             values[i] = ""
@@ -100,50 +123,66 @@ function renderModifiers(modSet) {
     modCha.textContent = values[5]
 }
 
-function applyRaceModifiers() {
-    // console.log(raceModifiers[characterRace][5])
-    console.log(`${previousRace} > ${characterRace}`)
-    for (i = 0; i < 6; i++) {
-        // remove modifiers from previous race
-        // console.log('prev', raceModifiers[previousRace][i])
-        characterStats[i] -= raceModifiers[previousRace][i]
-        // add modifiers from current race
-        // console.log('curr', raceModifiers[characterRace][i])
-        characterStats[i] += raceModifiers[characterRace][i]
+function applyRaceModifiers(modifierSet) {
+    if (modifierSet === 0) {
+        console.log("remove previous race modifiers")
+        console.log("current stats", characterStats)
+        console.log("base stats", baseCharacterStats)
+        characterStats = baseCharacterStats
+        console.log("revised stats", characterStats)
+        // for (let i = 0; i < 6; i++) {
+        //     characterStats[i] -= raceModifiers[previousRace][i]
+        // }
+        console.log("apply human modifiers")
+        for (let i = 0; i < 6; i++) {
+            characterStats[i] += raceModifiers[characterRace][i]
+        }
     }
 
-    renderStats()
-}
+    if (modifierSet === 1) {
+        console.log("remove previous race modifiers")
+        characterStats = baseCharacterStats
+        console.log("apply dwarf modifiers")
+        for (let i = 0; i < 6; i++) {
+            characterStats[i] += raceModifiers[characterRace][i]
+        }
+    }
 
-function renderBaseStats() {
-    baseStrength.textContent = characterStats[0]
-    baseDexterity.textContent = characterStats[1]
-    baseConstitution.textContent = characterStats[2]
-    baseIntelligence.textContent = characterStats[3]
-    baseWisdom.textContent = characterStats[4]
-    baseCharisma.textContent = characterStats[5]
-}
+    // // console.log(raceModifiers[characterRace][5])
+    // console.log(`${previousRace} > ${characterRace}`)
+    // for (i = 0; i < 6; i++) {
+    //     // remove modifiers from previous race
+    //     console.log('prev modifier', Number(raceModifiers[previousRace][i]))
+    //     characterStats[i] -= raceModifiers[previousRace][i]
+    //     console.log('minus', characterStats[i])
+    //     // add modifiers from current race
+    //     console.log('curr modifier', Number(raceModifiers[characterRace][i]))
+    //     characterStats[i] += Number(raceModifiers[characterRace][i])
+    //     console.log('plus', characterStats[i])
+    // }
 
-function renderStats() {
-    strength.textContent = characterStats[0]
-    dexterity.textContent = characterStats[1]
-    constitution.textContent = characterStats[2]
-    intelligence.textContent = characterStats[3]
-    wisdom.textContent = characterStats[4]
-    charisma.textContent = characterStats[5]
+    // renderStats()
 }
 
 function rollDice() {
     // get character race, so we can set modifiers later
     // chooseRace()
 
-    // roll base stats
+    // roll starting stats
     characterStats[0] = generateStat()
     characterStats[1] = generateStat()
     characterStats[2] = generateStat()
     characterStats[3] = generateStat()
     characterStats[4] = generateStat()
     characterStats[5] = generateStat()
+
+    // set BASE stats to starting stats
+    baseCharacterStats[0] = characterStats[0]
+    baseCharacterStats[1] = characterStats[1]
+    baseCharacterStats[2] = characterStats[2]
+    baseCharacterStats[3] = characterStats[3]
+    baseCharacterStats[4] = characterStats[4]
+    baseCharacterStats[5] = characterStats[5]
 
     // display stats onscreen
     renderBaseStats()
